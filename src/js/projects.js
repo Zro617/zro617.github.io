f47cb49ab145f9b9f6c80c900015a2a41f0a377b
Zro <zropotato@gmail.com>
2016-03-21 01:46:16 -0500

    whoops

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,7 +3,10 @@
   xhr.open("GET","https://scratch.mit.edu/site-api/projects/in/1000000/1/",true);
   xhr.responseType = "document";
   xhr.onload = function() {
-    if(this.status=='200'){var in=this.response.querySelectorAll("li"),i,out=document.querySelector("#scratch");for(i=0;i<in.length;i++)out.appendChild(in[i]);}
+    if(this.status=='200'){
+      var projects_in=this.response.querySelectorAll("li"),i,
+          projects_out=document.querySelector("#scratch");
+          for(i=0;i<projects_in.length;i++)projects_out.appendChild(projects_in[i]);}
   };
   xhr.send();
 })();
