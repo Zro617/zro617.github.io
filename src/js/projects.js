5f12ef71f01a5686211e2625500736812d908dc0
Zro <zropotato@gmail.com>
2016-03-21 01:53:17 -0500

    Update projects.js

--- src/js/projects.js
+++ src/js/projects.js
@@ -4,9 +4,18 @@
   xhr.responseType = "document";
   xhr.onload = function() {
     if(this.status=='200'){
-      var projects_in=this.response.querySelectorAll("li"),i,
+      var projects_in=this.response.querySelectorAll("li"),i,proj,img,
           projects_out=document.querySelector("#scratch");
-          for(i=0;i<projects_in.length;i++)projects_out.appendChild(projects_in[i]);}
+          for(i=0;i<projects_in.length;i++){
+            proj = projects_in[i];
+            img = proj.querySelector("img");
+            img["class"] = "thumbnail";
+            img["src"] = img["data-original"];
+            img["data-original"] = "";
+            proj.removeChild(querySelector("span.owner"));
+            projects_out.appendChild(proj);
+          }
+    }
   };
   xhr.send();
 })();
