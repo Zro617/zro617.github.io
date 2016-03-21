bbb67174e87df40ab6e7c091ef75e48e4ac9fed0
Zro <zropotato@gmail.com>
2016-03-21 00:25:24 -0500

    Create projects.js

---
+++ src/js/projects.js
@@ -0,0 +1,9 @@
+function(){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET","https://scratch.mit.edu/site-api/projects/in/1000000/1/",true);
+  xhr.responseType = "document";
+  xhr.onload = function() {
+    if(this.status=='200'){var in=this.response.querySelectorAll("li"),i,out=document.querySelector("#scratch");for(i=0;i<in.length;i++)out.appendChild(in[i]);}
+  };
+  xhr.send();
+})();
