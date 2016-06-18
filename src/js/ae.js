fc38a6c0bd12d299cd8198ac332187d899c8ccf9
Zro <zropotato@gmail.com>
2016-06-18 09:36:23 -0500

    Update ae.js

--- src/js/ae.js
+++ src/js/ae.js
@@ -1,7 +1,7 @@
 var regex = {
-	AE:/\Wae\W|add|follow|manage|curat(e|or)|invite|f4f|^projects$|(big(gest)?|large(st)?|huge(st)?) (studio|gallery)|(as|how) (many|much)|feed (me|us)|(can (i|we)|help (me|us)) (get( to)?|reach|add)|(get( to)?|reach|add|need) (\d+|every|any|all)? ?(thing|body|one|projects)|\d+ project/i,
-	notAE:/only|quality|good|awesome|fantastic|great|amazing|test|science|math|programming|computer|tech|info|advice/i,
+	AE:/\Wae\W|add|follow|manage|curat(e|or)|invite|f4f|^projects$|(big(gest)?|large(st)?|huge(st)?) (studio|gallery)|(as|how) (many|much)|feed (me|us)|(can (i|we)|help (me|us)) (get( to)?|reach|add)|(get( to)?|reach|add|need) (\d+|every|any|all)? ?(thing|body|one|projects)|(every|any)thing|\d+ project/gi,
+	notAE:/only|quality|good|awesome|fantastic|great|amazing|test|science|math|programming|computer|tech|info|advice/gi,
 	extras:null,
-	set:function(r){this.extras=new RegExp(r,"i");},
-	test:function(str){if(this.notAE.test(str))return 0;if(this.AE.test(str))return 1;if(this.extras && regex.extras.test(str))return 2;}
+	set:function(r){this.extras=new RegExp(r,"gi");},
+	test:function(str){if(this.notAE.test(str))return 0;if(this.AE.test(str))return 1;if(this.extras && regex.extras.test(str))return 2;return 0;}
 };
